904d162d3b136660c1fe20944f33edd2868b3f6e
ChoiHongYeon <164014605+ChoiHongYeon@users.noreply.github.com>
2024-06-05 22:11:03 +0900

    third commit

--- _20240605_1.java
+++ _20240605_1.java
@@ -5,26 +5,6 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
 
-        int max_length = numbers.length * (numbers.length - 1) / 2;
-        int nums[] = new int[max_length];
-
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                Arrays.sort(nums);
-                if (Arrays.binarySearch(nums, numbers[i] + numbers[j]) < 0)
-                    nums[0] = numbers[i] + numbers[j];
-            }
-        }
-
-        Arrays.sort(nums);
-        int nums_index = 0;
-        for (int i = 0; i < max_length; i++) {
-            if (nums[i] > 0) {
-                nums_index = i;
-                break;
-            }
-        }
-
         Arrays.sort(numbers);
         int zero = 0;
         for (int i = 0; i < numbers.length; i++) {
@@ -34,11 +14,38 @@ class Solution {
             }
         }
 
+        int nums[] = new int[numbers[numbers.length - 1] + numbers[numbers.length - 2] + 1];
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (Arrays.binarySearch(numbers, i - numbers[j]) >= 0) {
+                    if (numbers[j] * 2 != i) {
+                        nums[i] = i;
+                        break;
+                    } else {
+                        if (j + 1 < numbers.length) {
+                            if (numbers[j] == numbers[j + 1]) {
+                                nums[i] = i;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        Arrays.sort(nums);
+        int nums_index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                nums_index = i;
+                break;
+            }
+        }
+
         if (zero >= 2)
             nums_index--;
 
-        int[] answer = Arrays.copyOfRange(nums, nums_index, max_length);
-
+        int[] answer = Arrays.copyOfRange(nums, nums_index, nums.length);
         return answer;
 
     }
